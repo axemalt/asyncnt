a564be3c96a1575c736ba679b9ce2bb58c154d34
axemalt <moonblast3933@gmail.com>
2021-10-13 21:38:46 -0700

    made separate functions for getting captain and leaders of team

--- aiont/__init__.py
+++ aiont/__init__.py
@@ -13,11 +13,11 @@ import json
 import os
 
 
-with open(os.path.join(os.path.dirname(__file__), 'scrapers.json')) as f:
+with open(os.path.join(os.path.dirname(__file__), "scrapers.json")) as f:
     scrapers = json.load(f)["scrapers"]
 
 
-with open(os.path.join(os.path.dirname(__file__), 'cars.json')) as f:
+with open(os.path.join(os.path.dirname(__file__), "cars.json")) as f:
     data = json.load(f)["cars"]
     cars = {int(id): name for id, name in data.items()}
 
@@ -31,16 +31,9 @@ async def get_racer(username, scraper=None):
     requests = scraper or jsonpickle.decode(random.choice(scrapers))
     raw_data = await get_data(requests, f"https://nitrotype.com/racer/{username}")
 
-    regex_result = re.search(
-        r"RACER_INFO: \{\"(.*)\}",
-        raw_data.text.strip()
-    ).group(1)
+    regex_result = re.search(r"RACER_INFO: \{\"(.*)\}", raw_data.text.strip()).group(1)
 
-    data = json.loads(
-        '{"'
-        + regex_result
-        + "}"
-    )
+    data = json.loads('{"' + regex_result + "}")
 
     return Racer(data)
 
@@ -52,7 +45,7 @@ async def get_team(tag, scraper=None):
     data = json.loads(raw_data.content)
 
     return Team(data["data"])
-    
+
 
 class Racer:
     def __init__(self, data):
@@ -81,14 +74,10 @@ class Racer:
         self.wpm_high = data["highestSpeed"]
         self.friend_reqs_allowed = bool(data["allowFriendRequests"])
         self.looking_for_team = bool(data["lookingForTeam"])
-        self.created = date.fromtimestamp(data["createdStamp"]).strftime(
-            "%d %B %Y"
-        )
+        self.created = date.fromtimestamp(data["createdStamp"]).strftime("%d %B %Y")
 
         if data["carHueAngle"] == 0:
-            self.car = (
-                f'https://www.nitrotype.com/cars/{data["carID"]}_large_1.png'
-            )
+            self.car = f'https://www.nitrotype.com/cars/{data["carID"]}_large_1.png'
         else:
             self.car = f'https://www.nitrotype.com/cars/painted/{data["carID"]}_large_1_{data["carHueAngle"]}.png'
 
@@ -102,9 +91,9 @@ class Racer:
                 self.cars_owned += 1
             elif car[1] == "sold":
                 self.cars_sold += 1
-            self.cars_total += 1 
+            self.cars_total += 1
 
-    async def team(self):
+    async def get_team(self):
         return await get_team(self._team_tag)
 
 
@@ -113,36 +102,38 @@ class Team:
         info = data["info"]
         stats = data["stats"]
 
+        self._captain_username = info["username"]
+        self._leader_usernames = [
+            member["username"]
+            for member in data["members"]
+            if member["role"] == "officer"
+        ]
+
         for stat in stats:
-            board = stat['board']
+            board = stat["board"]
             speed = int(stat["typed"]) / 5 / stat["secs"] * 60
             accuracy = 100 - int(stat["errs"] / int(stat["typed"])) * 100
-                        
 
             setattr(self, f"{board}_pre", stat)
             setattr(self, f"{board}_races", stat["played"])
             setattr(self, f"{board}_speed", speed)
-            setattr(
-                self,
-                f"{board}_accuracy",
-                accuracy
-            )
+            setattr(self, f"{board}_accuracy", accuracy)
             setattr(
                 self,
                 f"{board}_points",
-                (
-                    stat["played"]
-                    * (100 + speed / 2)
-                    * accuracy
-                    / 100
-                )
+                (stat["played"] * (100 + speed / 2) * accuracy / 100),
             )
 
-        self.leaders = []
-        for elem in data["members"]:
-            if elem["role"] == "officer":
-                if elem["username"] == info["username"]:
-                    self.captain = Racer(elem)
+    async def get_captain(self):
+        return await get_racer(self._captain_username)
 
-                else:
-                    self.leaders.append(Racer(elem))
+    async def get_leaders(self, *, include_captain=False):
+        coruntines = []
+
+        for username in self._leader_usernames:
+            if username == self._captain_username and not include_captain:
+                pass
+            else:
+                coruntines.append(username)
+
+        return await asyncio.gather(*coruntines)
